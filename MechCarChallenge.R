f743280e41feda20d565bbf3176da832077901ba
Alexandra Curtis <ajcurtis916@gmail.com>
2022-04-19 21:08:44 -0700

    add deliverable 2

--- MechCarChallenge.R
+++ MechCarChallenge.R
@@ -3,10 +3,8 @@ setwd("C:/Users/acurt/Desktop/Data Analytics/M15 Statistics & R/r_analysis/Mecha
 getwd()
 
 #import dependencies
-library(jsonlite)
 library(dplyr)
 library(tidyr)
-library(ggplot2) 
 
 #read csv into table
 mpg_table<- read.csv(file='resources/MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
@@ -15,3 +13,18 @@ mpg_table<- read.csv(file='resources/MechaCar_mpg.csv',check.names=F,stringsAsFa
 model <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + 
      ground_clearance + AWD,data=mpg_table)
 summary(model)
+
+#regenerate multiple linear regression model with only statistically significant variables
+model <- lm(mpg ~ vehicle_length + ground_clearance,data=mpg_table)
+summary(model)
+
+#read csv into table
+suspension <- read.csv(file='resources/Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+#get mean, med, variance, and st dev of PSI column
+total_summary <- suspension %>%
+  summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+
+#groupby lot summary statistics
+lot_summary <- suspension %>% group_by(Manufacturing_Lot) %>% 
+  summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
